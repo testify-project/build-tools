9a29c2f17e8b84ecded5162240d1ee4b2f1d54e9
Sharmarke Aden <sharmarke.aden@gmail.com>
2017-01-14 13:22:05 -0800

    ### Changed - Made before install script executable - Added secret.tar.gz.enc which contains encrypted content

--- before_install.sh
+++ before_install.sh
@@ -18,7 +18,7 @@ set -e
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     echo "Decrypting Secrets"
-    openssl aes-256-cbc -K $encrypted_8da71b22b126_key -iv $encrypted_8da71b22b126_iv -in secrets.tar.gz.enc -out secrets.tar.gz -d
+    openssl aes-256-cbc -K $encrypted_a635af73de4d_key -iv $encrypted_a635af73de4d_iv -in secrets.tar.gz.enc -out secrets.tar.gz -d
     tar -xvzf secrets.tar.gz
 fi
 
